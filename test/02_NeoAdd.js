65072607f4bab622a57e0484d12c243f7d6b68b4
pwhitebe <pwhitebe@gmail.com>
2014-03-11 14:58:25 -0400

    Added test modules to connect to Neo4J
    Test modules now connect to neo4j, and there is a mini neo4j library
    that can be expanded-upon

--- test/02_NeoAdd.js
+++ test/02_NeoAdd.js
@@ -3,9 +3,11 @@ var testmodule = '../lib/neo4jconnect.js',
     should = require('should'),
     express = require('express'),
     app = express();
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 describe('Neo4JTester', function() {
-    require(testmodule)(app);
+    require(testmodule)(app, db);
     describe('Write to Database', function() {
         it('should write to the neo4j database, delete what was written, and then spit the string back', function(done) {
             request(app).

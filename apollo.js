bf172e3e1ab965af9984ecfebe650bfcb5817e51
pwhitebe <pwhitebe@gmail.com>
2014-03-13 16:53:30 -0400

    Added some Mongo code, but no tests yet
    Added some mongo code but no tests yet.  Will likely refactor the
    layout of the code to enhance readability and have tests reach hooks in
    the code better.

--- apollo.js
+++ apollo.js
@@ -3,14 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 var express = require('express'),
     app = express();
-    
+//configure express further
+app.configure(function(){
+  app.use(express.static(__dirname +'/server/views'));
+});
+
+app.get('/', function(req, res){
+    console.log('got a request for //n trying ' + __dirname +'/server/views/index.html');
+  res.sendfile(__dirname +'/server/views/index.html');
+});
+
 require('./lib/reverser')(app);
-require('./lib/neo4jconnect')(app, db);
+
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://localhost:7474');
+
+//require('./lib/neo4jconnect')(app, db);
+
+
+//require('./lib/mongouser');
+
 
 
 app.listen(8089);

2aa015f8658b745a5b8208b01463a78342d8f91c
pwhitebe <pwhitebe@gmail.com>
2014-03-14 13:32:08 -0400

    Updated routes
    Now it’s compiling and pulling from the controller and routes.  Angular
    is so EmVeeCee

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,6 @@ angular.module('app', ['ngResource','ngRoute']);
 
 angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
-    $routeProvider.when('/', {templateUrl: ' ', controller:'mainCtrl'});
+    $routeProvider.when('/', {templateUrl: '/partials/main', controller:'mainCtrl'});
     
 });
--- server/config/routes.js
+++ server/config/routes.js
@@ -3,8 +3,8 @@
 var index = require('../controllers/index');
 module.exports = function(app){
     
-    app.get('/partials/*', function(req,resp){
-        res.render('../../public/app/' +req.params); 
+    app.get('/partials/*', function(req,res){
+        res.render('partials/' +req.params); 
     });
     app.get('*', index.index); 
 };

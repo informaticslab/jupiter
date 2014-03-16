442c23a684b9c2ee056cee5f0902a42080fa743d
Ben Elder <belder@deloitte.com>
2014-03-15 20:54:46 -0400

    reformatted angular route configuration

--- public/app/app.js
+++ public/app/app.js
@@ -2,7 +2,8 @@ angular.module('app', ['ngResource','ngRoute']);
 
 angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
-    $routeProvider.when('/', {templateUrl: '/partials/main', controller:'mainCtrl'});
-    $routeProvider.when('/faq', {templateUrl: '/partials/faq', controller:'faqCtrl'});
-    $routeProvider.when('/node', {templateUrl: '/partials/node', controller:'nodeCtrl'});
+    $routeProvider
+        .when('/', {templateUrl: '/partials/main', controller:'mainCtrl'})
+        .when('/faq', {templateUrl: '/partials/faq', controller:'faqCtrl'})
+        .when('/node', {templateUrl: '/partials/node', controller:'nodeCtrl'});
 });

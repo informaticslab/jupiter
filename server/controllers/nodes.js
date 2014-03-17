3730e9033126ef5c7103202d618c6ad29645bff6
pwhitebe <pwhitebe@gmail.com>
2014-03-17 16:05:38 -0400

    Found pitfalls of node retrieval
    If you want anything related to the node you are getting, be prepared
    to make additional cypher queries.

--- server/controllers/nodes.js
+++ server/controllers/nodes.js
@@ -6,11 +6,34 @@ exports.getNodeById = function (req, res) {
             console.error('Error retreiving node from database:', err);
             res.send(500,'error retreiving node from database');
         } else {
-            //console.log('node looks like' , node);
+            //console.log('node looks like' , node.toJSON());
+            //var nodeblurb = node.toJSON();
+            //var labelURL = nodeblurb._data.labels;
             var nodedata = node.data;
-            console.log('Node retreived from database in JSON:', nodedata);
-            res.json(nodedata)
+            //var nodeid = node.id;
+            //console.log('Node label url:',  labelURL);
+            //console.log('Node id:', nodeid );
+            var query = [
+              'START n=node({nodeId}) ',
+              'RETURN labels(n)'
+            ].join('\n');
 
-        }
-    })
+            var params = {
+              nodeId:+req.params.id
+            };
+
+            neodb.db.query(query, params, function (err, results) {
+              if (err) {
+                  console.error('Error retreiving labels from database:', err);
+                res.send(500,'error retreiving label from database')
+              }
+                  console.log('results of label pull: ', results);
+              var labels = results.map(function (result) {
+                return result['labels(n)'];
+              });
+                nodedata.labels = labels
+                        res.json(nodedata)
+
+        })
+    }})
 };

f35c8f7f724cc22145b12070e178352ff37b3efe
Ben Elder <belder@deloitte.com>
2014-03-17 07:20:09 -0400

    beautified app.js using sublime text's codeformatter pluggin

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,14 @@
-angular.module('app', ['ngResource','ngRoute']);
-
+angular.module('app', ['ngResource', 'ngRoute']);
 angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
-    $routeProvider
-        .when('/', {templateUrl: '/partials/main', controller:'mainCtrl'})
-        .when('/faq', {templateUrl: '/partials/faq', controller:'faqCtrl'})
-        .when('/node', {templateUrl: '/partials/node', controller:'nodeCtrl'});
+    $routeProvider.when('/', {
+        templateUrl: '/partials/main',
+        controller: 'mainCtrl'
+    }).when('/faq', {
+        templateUrl: '/partials/faq',
+        controller: 'faqCtrl'
+    }).when('/node', {
+        templateUrl: '/partials/node',
+        controller: 'nodeCtrl'
+    });
 });

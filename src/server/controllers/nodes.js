50bb6805275add64a803b48925c5258909820b89
Ben Elder <belder@deloitte.com>
2014-03-19 16:59:38 -0400

    updated nodes.js by refactoring a bit and adding dummy relationship data

--- src/server/controllers/nodes.js
+++ src/server/controllers/nodes.js
@@ -1,7 +1,26 @@
-(function() {
-    var neodb = require('../lib/neo4jConnection');
+var neodb = require('../lib/neo4jConnection');
+var async = require('async');
+
+exports.getNodeById = function(req, res) {
+
+    var nodedata = {};
+
+    var setNodeAttributes = function(node) {
+        nodedata.name = node.data.name;
+        nodedata.id = req.params.id;
+
+        nodedata.attributes = [];
+
+        for (var prop in node.data) {
+            nodedata.attributes.push({
+                'key': prop,
+                'value': node.data[prop]
+            })
+        }
+    };
 
     var getLabels = function(id, callback) {
+
         var query = ['START n=node({nodeId}) ', 'RETURN labels(n)'].join('\n');
 
         var params = {
@@ -21,32 +40,43 @@
         });
     };
 
-    exports.getNodeById = function(req, res) {
-        neodb.db.getNodeById(req.params.id, function(err, node) {
-            if (err) {
-                console.error('Error retreiving node from database:', err);
-                res.send(500, 'error retreiving node from database');
-            } else {
+    var getRelationships = function(node, callback) {
+        var relationships = {
+            systems: [{
+                id: 2,
+                name: 'System 2'
+            }, {
+                id: 3,
+                name: 'System 3'
+            }],
+            dataSets: [{
+                id: 2,
+                name: 'Set 2'
+            }, {
+                id: 3,
+                name: 'Set 3'
+            }]
+        };
 
-                var nodedata = {
-                    name: node.data.name,
-                    id: req.params.id
-                };
-
-                nodedata.attributes = [];
-
-                for (var prop in node.data) {
-                    nodedata.attributes.push({
-                        'key': prop,
-                        'value': node.data[prop]
-                    })
-                }
-
-                getLabels(req.params.id, function(labels) {
-                    nodedata.labels = labels;
-                    res.json(nodedata)
-                });
-            }
-        })
+        callback(relationships);
     };
-})();
+
+    var nodeRetrieved = function(err, node) {
+        if (err) {
+            console.error('Error retreiving node from database:', err);
+            res.send(500, 'error retreiving node from database');
+        } else {
+
+            setNodeAttributes(node);
+            getLabels(req.params.id, function(labels) {
+                nodedata.labels = labels;
+                getRelationships(node, function(relationships) {
+                    nodedata.relationships = relationships;
+                    res.json(nodedata);
+                });
+            });
+        }
+    };
+
+    neodb.db.getNodeById(req.params.id, nodeRetrieved);
+};

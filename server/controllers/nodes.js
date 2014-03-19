3517f3e0318ec760304e1dadbd877a8d112306c2
Ben Elder <belder@deloitte.com>
2014-03-19 11:09:17 -0400

    separated out the query for a node's labels for cleaner code

--- server/controllers/nodes.js
+++ server/controllers/nodes.js
@@ -1,38 +1,50 @@
 var neodb = require('../lib/neo4jConnection');
 
-exports.getNodeById = function (req, res) { 
-    var node = neodb.db.getNodeById(req.params.id, function(err, node){
+var getLabels = function(id, callback) {
+    var query = ['START n=node({nodeId}) ', 'RETURN labels(n)'].join('\n');
+
+    var params = {
+        nodeId: +id
+    };
+
+    neodb.db.query(query, params, function(err, results) {
+        if (err) {
+            console.error('Error retreiving labels from database:', err);
+        }
+
+        var labels = results.map(function(result) {
+            return result['labels(n)'];
+        });
+
+        callback(labels);
+    });
+};
+
+exports.getNodeById = function(req, res) {
+    neodb.db.getNodeById(req.params.id, function(err, node) {
         if (err) {
             console.error('Error retreiving node from database:', err);
-            res.send(500,'error retreiving node from database');
+            res.send(500, 'error retreiving node from database');
         } else {
+
             var nodedata = {
-                name : node.data.name
-                ,id : req.params.id
-            };
-            nodedata.attributes=[];
-            for (var propt in node.data){
-                nodedata.attributes.push({'key':propt, 'value':node.data[propt]})
-            }
-            var query = [
-              'START n=node({nodeId}) ',
-              'RETURN labels(n)'
-            ].join('\n');
-
-            var params = {
-              nodeId:+req.params.id
+                name: node.data.name,
+                id: req.params.id
             };
 
-            neodb.db.query(query, params, function (err, results) {
-            if (err) {
-                console.error('Error retreiving labels from database:', err);
-            res.send(500,'error retreiving label from database')
+            nodedata.attributes = [];
+
+            for (var prop in node.data) {
+                nodedata.attributes.push({
+                    'key': prop,
+                    'value': node.data[prop]
+                })
             }
-              //console.log('results of label pull: ', results);
-            nodedata.labels = results.map(function (result) {
-                return result['labels(n)'];
+
+            getLabels(req.params.id, function(labels) {
+                nodedata.labels = labels;
+                res.json(nodedata)
             });
-            res.json(nodedata)        
-            })
-    }})
+        }
+    })
 };

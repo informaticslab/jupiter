919ab75569068fb0447f9f45a204dc856178972c
Ben Elder <belder@deloitte.com>
2014-03-19 23:05:20 -0400

    added ngAnimate to app

--- src/public/app/app.js
+++ src/public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngResource', 'ngRoute']);
+angular.module('app', ['ngResource', 'ngRoute', 'ngAnimate']);
 angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider.when('/', {

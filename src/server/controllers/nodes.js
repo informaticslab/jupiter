f768b0b6ea1eb0ba20b3c5fa1f25c3a0e9f7d271
pwhitebe <pwhitebe@gmail.com>
2014-03-24 15:52:49 -0400

    Updating syntax of node build in relationships

--- src/server/controllers/nodes.js
+++ src/server/controllers/nodes.js
@@ -17,120 +17,53 @@ exports.getRelationsForNode = function(req, res) {
     neodb.db.query(query, params, function(err, r) {
         if (err) {
             console.error('Error retreiving labels from database:', err);
-            res.err()
-        } else
-        {
-         // results.forEach(function(entry) {
+            res.err(err);
+        } else {
+            var allLabels = _.map(r, function(i){return i.childLabels});
+            var distinctLabels = _.union.apply(_, allLabels);
 
-         // //   console.log(entry);
-         //  //  relationships.push(entry);
-         //    entry.childLabels.forEach(function(childLabel){
+            var toRet = [];
 
-         //        var labelItem = relationships[childLabel];
-         //        if(labelItem == null){ //please create a new label item
-         //            // var relType = {
-         //            //         'id': entry.relId
-         //            //         ,'relName': entry.relType                            
-         //            //         ,'nodeId': entry.childId
-         //            //         ,'nodeName': entry.childName                           
-         //            //     }                  
-         //            // labelItem = [];
-         //            // labelItem.relTypes = [relType];
-         //            // labelItem = {'foo':'bar'};
-         //            // relationships[childLabel] = labelItem
-         //                            labelItem = {'foo':'bar'};
-         //            relationships[childLabel] = labelItem;
-         //        }
-         //        else
-         //        {
-
-         //           //  var relType = {
-         //           //          'id': entry.relId
-         //           //          ,'relName': entry.relType                            
-         //           //          ,'nodeId': entry.childId
-         //           //          ,'nodeName': entry.childName                           
-         //           //      }                                 
-         //           // labelItem.relTypes.push(relType);
-
-         //        }
-            
-         //    })
-            
-         // })
-    var allLabels = _.map(r, function(i){return i.childLabels});
-   var distinctLabels = _.union.apply(_, allLabels);
-   
-   var toRet = [];
-   
-   _.each(distinctLabels, function(a,b,c){
-       var labelToAdd = {
-           'name': a,
-           'relTypes' : []
-       };
-       
-       var labeled = _.filter(r, function(i){
-           return _.contains(i.childLabels, a);
-       });
-       
-       var mappedRelTypes = _.map(labeled, function(p){return p.relType;});        
-       var distinctRelTypes = _.union.apply(_, mappedRelTypes);
-      
-       _.each(distinctRelTypes, function(x,y,z){
-           var relTypeToAdd = {
-               'name': x 
-           };
+            _.each(distinctLabels, function(a){
+               var labelToAdd = {
+                   'name': a,
+                   'relTypes' : []
+               };
+               
+               var labeled = _.filter(r, function(i){
+                   return _.contains(i.childLabels, a);
+               });
+               
+               var mappedRelTypes = _.map(labeled, function(p){return p.relType;});        
+               var distinctRelTypes = _.union.apply(_, mappedRelTypes);
+              
+               _.each(distinctRelTypes, function(x,y,z){
+                   var relTypeToAdd = {
+                       'name': x ,
+                       'nodes': []
+                   };
+                   
+                   _.chain(labeled)
+                   .filter(function(i){
+                       return i.relType === x;
+                   })
+                   .each(function(a){
+                       relTypeToAdd.nodes.push( {
+                           'id' : a.childId,
+                           'name' : a.childName
+                       });
+                   });
            
-           relTypeToAdd.nodes = _.filter(labeled, function(i){
-               return i.relType === x;
-           });
-           
-           labelToAdd.relTypes.push(relTypeToAdd);
-       });
-       
-       toRet.push(labelToAdd);
-   });
-        res.json(toRet);
-        console.log(toRet);
+                   labelToAdd.relTypes.push(relTypeToAdd);
+               });
+               
+               toRet.push(labelToAdd);
+            });
+                res.json(toRet);
+              //  console.log(toRet);
         
     }
     });
-
-    // var systems = [{
-    //     id: 2,
-    //     name: 'System 2'
-    // }, {
-    //     id: 3,
-    //     name: 'System 3'
-    // }];
-
-    // var datasets = [{
-    //     id: 2,
-    //     name: 'Set 2'
-    // }, {
-    //     id: 3,
-    //     name: 'Set 3'
-    // }];
-
-    // var dataElements = [{
-    //     id: 45,
-    //     name: 'Element 2'
-    // }, {
-    //     id: 56,
-    //     name: 'Element 3'
-    // }];
-
-    // var relationships = [{
-    //     type: 'Systems',
-    //     values: systems
-    // }, {
-    //     type: 'Data Sets',
-    //     values: datasets
-    // }, {
-    //     type: 'Data Elements',
-    //     values: null
-    // }];
-
-    // res.json(relationships);
 }
 
 

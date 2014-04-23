4668e7a1d0c33e379f3bdd4de553819782fd012e
pwhitebe <pwhitebe@gmail.com>
2014-04-23 11:59:01 -0400

    Added the unCamelCase filter to search and details
    There is a new “unCamelCase” filter that uses some regex to pull the
    camel case and turn them human readable.

---
+++ src/public/app/filters/unCamelCase.js
@@ -0,0 +1,30 @@
+angular.module('apolloApp').filter('unCamelCase', function() {
+return function(input) {
+/**
+ * Turns someCrazyName into Some Crazy Name
+ * Decent job of acroynyms:
+ * ABCAcryonym => ABC Acryoynm
+ * xmlHTTPRequest => Xml HTTP Request
+ */
+
+	return input
+		// insert a space between lower & upper
+		.replace(/([a-z])([A-Z])/g, '$1 $2')
+		// space before last upper in a sequence followed by lower
+		.replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+		// uppercase the first character
+		.replace(/^./, function(input){ return input.toUpperCase(); })
+
+
+  // input = input || '';
+  // var out = "";
+  // for (var i = 0; i < input.length; i++) {
+  //   out = input.charAt(i) + out;
+  // }
+  // // conditional based on optional argument
+  // if (uppercase) {
+  //   out = out.toUpperCase();
+  // }
+  // return out;
+};
+})

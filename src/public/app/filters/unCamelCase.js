3c34a091a19b61aa7be1ecba4471934f85085dbd
pwhitebe <pwhitebe@gmail.com>
2014-04-23 12:01:07 -0400

    added null check to filter
    Now we don’t get errors if there is an empty item.

--- src/public/app/filters/unCamelCase.js
+++ src/public/app/filters/unCamelCase.js
@@ -7,14 +7,20 @@ return function(input) {
  * xmlHTTPRequest => Xml HTTP Request
  */
 
-	return input
-		// insert a space between lower & upper
-		.replace(/([a-z])([A-Z])/g, '$1 $2')
-		// space before last upper in a sequence followed by lower
-		.replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
-		// uppercase the first character
-		.replace(/^./, function(input){ return input.toUpperCase(); })
-
+  if(input != null)
+  {
+  	return input
+  		// insert a space between lower & upper
+  		.replace(/([a-z])([A-Z])/g, '$1 $2')
+  		// space before last upper in a sequence followed by lower
+  		.replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+  		// uppercase the first character
+  		.replace(/^./, function(input){ return input.toUpperCase(); })
+  }
+  else
+  {
+    return null
+  }
 
   // input = input || '';
   // var out = "";

6d83d570df6a44d21d94c2260ae6e7b94015af78
sanjithdavid <sdavid@deloitte.com>
2014-04-24 09:49:51 -0400

    Linkage and title fix
    Fixed linkage title, added tooltip and minor updates to linkage node
    design

--- src/public/app/controllers/linkageCtrl.js
+++ src/public/app/controllers/linkageCtrl.js
@@ -1,4 +1,14 @@
-angular.module('apolloApp').controller('linkageCtrl', function($scope, $routeParams){
+angular.module('apolloApp').controller('linkageCtrl', function($scope, $routeParams, $resource){
 	$scope.$parent.q = 'explore';
     $scope.nodeId = $routeParams.id;
+
+    var node = $resource('/apollo/api/node/:id', {
+        id: '@id'
+    });
+
+    $scope.node = node.get({
+        id: $routeParams.id
+    });
+    
+
 });

c3e46f926e90075daaafdf3b16ae421bffe86cf0
Ben Elder <belder@deloitte.com>
2014-04-28 15:33:39 -0400

    split out rootCtrl into its own file

--- src/public/app/controllers/mainCtrl.js
+++ src/public/app/controllers/mainCtrl.js
@@ -2,24 +2,3 @@ angular.module('apolloApp').controller('mainCtrl', function($scope){
 	$scope.$parent.q = 'home';
     $scope.rays = 'sunshine';
 });
-
-angular.module('apolloApp').controller('rootCtrl', function($scope, $http){
-    $scope.q = 'home';
-    $scope.loginuser = 'guest';
-    $http.get('build.json')
-      .then(function(res){
-      	
-      	var build = res.data
-         $scope.buildNumber = res.data.buildNumber;              
-       });
-    $scope.toCapitalizedWords  = function toCapitalizedWords(name) {
-    //var words = name.match(/[A-Za-z][a-z]*/g);
-    var words = name.match(/^[a-z]+|[A-Z][a-z]*/g);
-    return words.map(capitalize).join(" ");
-    };
-
-    function capitalize(word) {
-    return word.charAt(0).toUpperCase() + word.substring(1);
-    }
-
-});
---
+++ src/public/app/controllers/rootCtrl.js
@@ -0,0 +1,27 @@
+angular.module('apolloApp').controller('rootCtrl', function($scope, $http){
+    
+    $scope.q = 'home';
+    $scope.loginuser = 'guest';
+    $scope.queryString = '';
+    
+    $http.get('build.json')
+      .then(function(res){
+      	
+      	var build = res.data
+         $scope.buildNumber = res.data.buildNumber;              
+       });
+    
+    $scope.toCapitalizedWords  = function toCapitalizedWords(name) {
+      //var words = name.match(/[A-Za-z][a-z]*/g);
+      var words = name.match(/^[a-z]+|[A-Z][a-z]*/g);
+      return words.map(capitalize).join(" ");
+    };
+
+    function capitalize(word) {
+      return word.charAt(0).toUpperCase() + word.substring(1);
+    }
+
+    $scope.redirectToSearch = function(){
+       window.location =  '/apollo/#/search/' + $scope.queryString;
+    };
+});

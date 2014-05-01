1163f3ba79603a90bfb80de1a1afb7967768ad37
Madhavi Tammineni <madhavi.tammineni@gmail.com>
2014-05-01 11:23:14 -0400

    Displaying the Node Details in two columns

--- src/public/app/controllers/nodeCtrl.js
+++ src/public/app/controllers/nodeCtrl.js
@@ -19,31 +19,20 @@ angular.module('apolloApp').controller('nodeCtrl', function($scope, $resource, $
     //     id: $routeParams.id
     // });
 
-    var splitArr1 = [];
-    var splitArr2 = [];
+    $scope.splitArr1 = [];
+    $scope.splitArr2 = [];
     var nodeDetails = $http.get('/apollo/api/node/' + $routeParams.id)
         .success(function(data){
             $scope.node = data;
-            // window.alert("The total number of attributes are:"+$scope.node.attributes.length);
-           
             var len = $scope.node.attributes.length;
             for (var i = 0; i< len; i++) {
                 if(i%2 == 0){
-                    splitArr1.push($scope.node.attributes[i]);
+                    $scope.splitArr1.push($scope.node.attributes[i]);
                 }
                 else{
-                    splitArr2.push($scope.node.attributes[i]);
+                    $scope.splitArr2.push($scope.node.attributes[i]);
                 }
             };
-
-            $scope.splitArr1 = splitArr1.slice(0);
-            $scope.splitArr2 = splitArr2;
-            // window.alert("The len of splitArr1 is:"+$scope.splitArr1.length);
-            // window.alert("The len of splitArr2 is:"+$scope.splitArr2.length);
-
-            window.alert("Test printing one of the values from splitArr1. The ID is:"+$scope.splitArr1);
-
-
         });
 
    

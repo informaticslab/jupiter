1daa3fcbfb2c94a6b391f37a0759b51dd00e3d09
Madhavi Tammineni <madhavi.tammineni@gmail.com>
2014-05-01 15:44:39 -0400

    Updated node view

--- src/public/app/controllers/nodeCtrl.js
+++ src/public/app/controllers/nodeCtrl.js
@@ -29,29 +29,27 @@ angular.module('apolloApp').controller('nodeCtrl', function($scope, $resource, $
             var len = $scope.node.attributes.length;
             for (var i = 0; i< len; i++) {
                
-                if((($scope.node.attributes[i].key != 'name') || ($scope.node.attributes[i].key != 'id') || ($scope.node.attributes[i].key != 'fullNameCIO') || ($scope.node.attributes[i].key != 'fullName')) && (i%2 == 0)) {
-                    $scope.splitArr1.push($scope.node.attributes[i]);
+                if(($scope.node.attributes[i].key == 'name') || ($scope.node.attributes[i].key == 'id') || ($scope.node.attributes[i].key == 'fullNameCIO') || ($scope.node.attributes[i].key == 'fullName')) {
+                    // do nothing
                 }
-                else if(i%2 != 0){
-                    $scope.splitArr2.push($scope.node.attributes[i]);
+                else{
+                    if(i%2 == 0){
+                        $scope.splitArr1.push($scope.node.attributes[i]);
+                    }
+                    else{
+                        $scope.splitArr2.push($scope.node.attributes[i]);
+                    }
+                    
                 }
 
             };
 
         });
 
-    
+   
     $scope.relations = relations.query({
         id: $routeParams.id
-    },function(data){
-        for (var i = 0; i < data.length; i++)
-        {
-            if (data[i].name == 'Tag')
-            {
-                $scope.nodeTags = data[i].relTypes[0].nodes;
-                $scope.hasTags = true;
-            }
-        }
+    },function(){
         $scope.contentLoading = false;
     });
 

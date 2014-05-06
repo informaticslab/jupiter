f0bb3bb255d675ab2c20808730311e51fd5f453b
sanjithdavid <sdavid@deloitte.com>
2014-05-06 14:23:01 -0400

    portal statistics - removed console log statement

--- src/public/app/controllers/mainCtrl.js
+++ src/public/app/controllers/mainCtrl.js
@@ -2,18 +2,17 @@ angular.module('apolloApp').controller('mainCtrl', function($scope,$resource){
 	$scope.$parent.q = 'home';
     $scope.rays = 'sunshine';
 
+
+
 	var portalstats = $resource('/apollo/api/stats', {
 	});
 
+
 	var stats = portalstats.query({
-	},function(result,r1){
+	},function(result){
 	if (!result.nullset)
 	{
-		console.log(r1);
 		$scope.statsarr=result;
-		result.forEach(function(d) {
-		//console.log(d.label[0],d.count);
-		});
 	}
 
 	});

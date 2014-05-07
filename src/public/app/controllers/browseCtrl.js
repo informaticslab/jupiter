717120884a5cc8ded3b839b82f3cddcc606ad056
Madhavi Tammineni <madhavi.tammineni@gmail.com>
2014-05-07 12:52:00 -0400

    Back to Top feature added

--- src/public/app/controllers/browseCtrl.js
+++ src/public/app/controllers/browseCtrl.js
@@ -1,3 +1,8 @@
-angular.module('apolloApp').controller('browseCtrl', function($scope){
+angular.module('apolloApp').controller('browseCtrl', function($scope, $location, $anchorScroll){
     $scope.$parent.q = 'explore';
+
+    //function for scrolling to the top
+    $scope.goToTop = function(){
+    	$anchorScroll();
+    }
 });

5c35a7a1dc13b6dcf92e69d7dff588c2075bd9b5
Madhavi Tammineni <madhavi.tammineni@gmail.com>
2014-05-15 13:13:48 -0400

    Quick links provided to the different sections on the Foodborne Illness (Quick Guide Details) Page

--- src/public/app/controllers/quickGuideCtrl.js
+++ src/public/app/controllers/quickGuideCtrl.js
@@ -1,8 +1,15 @@
-angular.module('apolloApp').controller('quickGuideCtrl', function($scope, $location, $anchorScroll){
+angular.module('apolloApp').controller('quickGuideCtrl', function($scope, $location, $anchorScroll, $routeParams){
     $scope.$parent.q = 'explore';
 
 //function for scrolling to the top
     $scope.goToTop = function(){
+    	$anchorScroll();
+    }
+
+    $scope.goToSection = function(anchorId){
+    	//scroll to overview
+    	console.log("The selected anchor's id is: "+anchorId);
+    	$location.hash(anchorId);
     	$anchorScroll();
     }
 });

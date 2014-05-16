67ffca76c5ed98658f121bc09ea4dc0b09a2966c
pwhitebe <pwhitebe@gmail.com>
2014-05-16 09:18:06 -0400

    Added functions to update pagination upon checkbox filtering

--- src/public/app/controllers/searchCtrl.js
+++ src/public/app/controllers/searchCtrl.js
@@ -58,12 +58,70 @@ angular.module('apolloApp').controller('searchCtrl', function($scope, $resource,
   $scope.entryLimit = 10;
   $scope.maxSize = 10;
 
-  // $scope.setPage = function (pageNo) {
-  //   $scope.currentPage = pageNo;
-  // };
-  $scope.$watch($scope.checkedLabels.Program, function() {
-        console.log('program changed');
-    });
+
+   $scope.filterclick = function(){
+    $timeout(function(){ 
+        //timeout to avoid race condition.  Could add a watch, but they don't seem to recognize labelCount status :-/
+        
+        //console.log('someone clicked a filter.  Program is currently ' + $scope.checkedLabels.Program);
+        //check all checkboxes, if all are unchecked, then reset totalItems count for pagination
+            if (!$scope.checkedLabels.Program && !$scope.checkedLabels.SurveillanceSystem && !$scope.checkedLabels.Registry 
+                                && !$scope.checkedLabels.HealthSurvey&& !$scope.checkedLabels.Tool&& !$scope.checkedLabels.Dataset&& !$scope.checkedLabels.DataStandard
+                                && !$scope.checkedLabels.Collaborative && !$scope.checkedLabels.Organization && !$scope.checkedLabels.Tag)
+            {
+                $scope.totalItems =  $scope.nodes.length;
+            }
+            else 
+            {
+                //check all checkboxes and add to the total count depending on what's checked.
+                var filteredTotalItems = 0;
+                if ($scope.checkedLabels.Program)
+                {
+                    filteredTotalItems = filteredTotalItems +  $scope.labelCounts.Program;
+                }
+                if ($scope.checkedLabels.SurveillanceSystem)
+                {
+                    filteredTotalItems = filteredTotalItems +  $scope.labelCounts.SurveillanceSystem;
+                }
+                if ($scope.checkedLabels.Registry)
+                {
+                    filteredTotalItems = filteredTotalItems +  $scope.labelCounts.Registry;
+                }
+                if ($scope.checkedLabels.HealthSurvey)
+                {
+                    filteredTotalItems = filteredTotalItems +  $scope.labelCounts.HealthSurvey;
+                }
+                if ($scope.checkedLabels.Tool)
+                {
+                    filteredTotalItems = filteredTotalItems +  $scope.labelCounts.Tool;
+                }
+                if ($scope.checkedLabels.Dataset)
+                {
+                    filteredTotalItems = filteredTotalItems +  $scope.labelCounts.Dataset;
+                }
+                if ($scope.checkedLabels.DataStandard)
+                {
+                    filteredTotalItems = filteredTotalItems +  $scope.labelCounts.DataStandard;
+                }
+                if ($scope.checkedLabels.Collaborative)
+                {
+                    filteredTotalItems = filteredTotalItems +  $scope.labelCounts.Collaborative;
+                }
+                if ($scope.checkedLabels.Organization)
+                {
+                    filteredTotalItems = filteredTotalItems +  $scope.labelCounts.Organization;
+                }
+                if ($scope.checkedLabels.Tag)
+                {
+                    filteredTotalItems = filteredTotalItems +  $scope.labelCounts.Tag;
+                }
+                $scope.totalItems =  filteredTotalItems;
+            }        
+
+
+        }, 10);
+    
+  }
 
 
 

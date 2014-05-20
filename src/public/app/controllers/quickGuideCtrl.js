997d468a6b556a5319d33b4ba24b045625ef7603
Madhavi Tammineni <madhavi.tammineni@gmail.com>
2014-05-20 14:35:53 -0400

    scroll conflict fixed between the "On the Page" section and back to top links

--- src/public/app/controllers/quickGuideCtrl.js
+++ src/public/app/controllers/quickGuideCtrl.js
@@ -1,15 +1,16 @@
 angular.module('apolloApp').controller('quickGuideCtrl', function($scope, $location, $anchorScroll, $routeParams){
     $scope.$parent.q = 'explore';
 
-//function for scrolling to the top
-    $scope.goToTop = function(){
-    	$anchorScroll();
-    };
+	//function for scrolling to the top
+    // $scope.goToTop = function(){
+    // 	$anchorScroll();
+    // }
 
     $scope.goToSection = function(anchorId){
+    	
     	$location.hash(anchorId);
     	$anchorScroll();
-    };
+    }
 });
 
 function goBack()

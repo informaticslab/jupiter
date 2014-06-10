158f811e78143d415ceaf234ee996c3c25768ea3
Ben Elder <belder@deloitte.com>
2014-06-10 17:40:09 -0400

    binding selected nodes on page load if values exist in route

--- src/public/app/controllers/advancedSearchCtrl.js
+++ src/public/app/controllers/advancedSearchCtrl.js
@@ -1,26 +1,34 @@
-angular.module('apolloApp').controller('advancedSearchCtrl', function($scope, $http, $routeParams, $resource){
-	$scope.$parent.q = 'explore';
+angular.module('apolloApp').controller('advancedSearchCtrl', function($scope, $http, $routeParams, $resource) {
+    $scope.$parent.q = 'explore';
     $scope.nodeId = $routeParams.id;
-    
-	$scope.getNodes = function(val) {
-	    return $http
-	    	.get('/apollo/api/node/searchByName/' + val)
-		    .then(function(res){
-	      		var nodes = [];
-		      	angular.forEach(res.data, function(item){
-		        	nodes.push(item);
-		      	});
-		      return nodes; 
-		    });
-  	};
-
-  	$scope.itemSelectedA = function($item, $model, $label)
-  	{
-		$scope.nodeAId = $item.id;
-  	};
-  	
-	$scope.itemSelectedB = function($item, $model, $label)
-  	{
-		$scope.nodeBId = $item.id;
-  	};
+    $scope.getNodes = function(val) {
+        return $http.get('/apollo/api/node/searchByName/' + val).then(function(res) {
+            var nodes = [];
+            angular.forEach(res.data, function(item) {
+                nodes.push(item);
+            });
+            return nodes;
+        });
+    };
+    var id = $routeParams.id;
+    if (id) {
+        var queryVals = id.split("-");
+        if (queryVals.length) {
+            $http.get('/apollo/api/node/name/' + id.split("-")[0]).then(function(res) {
+                $scope.nodeA = res.data;
+            });
+            $http.get('/apollo/api/node/name/' + id.split("-")[1]).then(function(res) {
+                $scope.nodeB = res.data;
+            });
+            $scope.nodeAId = id.split("-")[0];
+            $scope.nodeBId = id.split("-")[1];
+            $scope.hops = id.split("-")[2];
+        }
+    }
+    $scope.itemSelectedA = function($item, $model, $label) {
+        $scope.nodeAId = $item.id;
+    };
+    $scope.itemSelectedB = function($item, $model, $label) {
+        $scope.nodeBId = $item.id;
+    };
 });

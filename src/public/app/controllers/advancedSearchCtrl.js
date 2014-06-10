0e3cdf9556f9858a723b0c6e0be0b80f5f7a4277
Ben Elder <belder@deloitte.com>
2014-06-10 14:57:17 -0400

    updated the advancedSearchCtrl with typeahead handler

--- src/public/app/controllers/advancedSearchCtrl.js
+++ src/public/app/controllers/advancedSearchCtrl.js
@@ -1,6 +1,17 @@
-angular.module('apolloApp').controller('advancedSearchCtrl', function($scope, $routeParams, $resource){
+angular.module('apolloApp').controller('advancedSearchCtrl', function($scope, $http, $routeParams, $resource){
 	$scope.$parent.q = 'explore';
     $scope.nodeId = $routeParams.id;
     
+	$scope.getNodes = function(val) {
+	    return $http
+	    	.get('/apollo/api/node/searchByName/' + val)
+		    .then(function(res){
+	      		var nodes = [];
+		      	angular.forEach(res.data, function(item){
+		        	nodes.push(item);
+		      	});
+		      return nodes; 
+		    });
+  	};
 
 });

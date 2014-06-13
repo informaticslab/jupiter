742c4fe5dc5d03ba094fc9670060e6366562aec3
pwhitebe <pwhitebe@gmail.com>
2014-06-13 14:47:19 -0400

    Analytics Key Update and Prelim steps to ngTopNavBar added.  Next need to adjust focus based on calling item.

---
+++ src/public/app/directives/ngTopnavbar.js
@@ -0,0 +1,22 @@
+angular.module('apolloApp').directive('ngTopnavbar', function () {
+    return {
+      restrict: 'A',
+      template: '<ul class="nav nav-pills"> ' +
+                  '<li><a href="#/quickGuide/">Quick Guide</a></li> ' +
+                  '<li class="active"><a href="#/browse/">Browse</a></li> ' +
+                  '<li><a href="#/search/">Search</a></li> ' +
+                  '<li><a href="#/advancedSearch/">Relationship Explorer</a></li> ' +
+                  '<li><a href="" class="inactive">Favorites</a></li> ' +
+                  '<li><a href="#/inTheLab/">In the Lab</a></li> ' +
+                '</ul>'
+      // '<div ng-repeat="site in browseHistory.sites">' +
+      //             '<a ng-click="goToTop()" href="{{site.url}}">' +
+      //                 '{{site.name}}'+
+      //             '</a' +
+      //           '</div>'
+                ,
+      link: function (scope, elem, attrs) {
+        //console.log("Recognized the historybar directive usage");
+      }
+    }
+});

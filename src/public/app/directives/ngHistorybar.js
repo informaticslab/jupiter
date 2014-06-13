92ddeff8e677c42da88ce27a07333152d9da0054
pwhitebe <pwhitebe@gmail.com>
2014-06-13 09:57:59 -0400

    Updated history template, now eats more of the sidebar in every jade page.

--- src/public/app/directives/ngHistorybar.js
+++ src/public/app/directives/ngHistorybar.js
@@ -1,11 +1,20 @@
 angular.module('apolloApp').directive('ngHistorybar', function () {
     return {
       restrict: 'A',
-      template: '<div ng-repeat="site in browseHistory.sites">' +
-                  '<a ng-click="goToTop()" href="{{site.url}}">' +
-                      '{{site.name}}'+
-                  '</a' +
-                '</div>',
+      template: '<div class="blue_side"> ' + 
+                  '<div class="blue_side_text">Your Exploration History:</div> ' +
+                    '<div class="sidebar_now"> ' +
+                      '<div class="sidebar_repeat"> ' +
+                        '<div ng-repeat="site in browseHistory.sites"><a ng-click="goToTop()" href="{{site.url}}">{{site.name}}</a></div> ' +
+                      '</div> '+
+                    '</div> '+
+                '</div>'
+      // '<div ng-repeat="site in browseHistory.sites">' +
+      //             '<a ng-click="goToTop()" href="{{site.url}}">' +
+      //                 '{{site.name}}'+
+      //             '</a' +
+      //           '</div>'
+                ,
       link: function (scope, elem, attrs) {
         //console.log("Recognized the historybar directive usage");
       }

beca00e0bb6df6496ad0a1b1f2fccdd8ccde3c4c
pwhitebe <pwhitebe@gmail.com>
2014-06-18 13:26:53 -0400

    Removed outdated files.

--- src/public/app/d3/d3-sysTreeSimple.js
+++ src/public/app/d3/d3-sysTreeSimple.js
@@ -1,70 +1,198 @@
+
+
+// var treeData = {
+//     name: "/",
+//     contents: [
+//         {
+//             name: "Applications",
+//             contents: [
+//                 { name: "Mail.app" },
+//                 { name: "iPhoto.app" },
+//                 { name: "Keynote.app" },
+//                 { name: "iTunes.app" },
+//                 { name: "XCode.app" },
+//                 { name: "Numbers.app" },
+//                 { name: "Pages.app" }
+//             ]
+//         },
+//         {
+//             name: "System",
+//             contents: []
+//         },
+//         {
+//             name: "Library",
+//             contents: [
+//                 {
+//                     name: "Application Support",
+//                     contents: [
+//                         { name: "Adobe" },
+//                         { name: "Apple" },
+//                         { name: "Google" },
+//                         { name: "Microsoft" }
+//                     ]
+//                 },
+//                 {
+//                     name: "Languages",
+//                     contents: [
+//                         { name: "Ruby" },
+//                         { name: "Python" },
+//                         { name: "Javascript" },
+//                         { name: "C#" }
+//                     ]
+//                 },
+//                 {
+//                     name: "Developer",
+//                     contents: [
+//                         { name: "4.2" },
+//                         { name: "4.3" },
+//                         { name: "5.0" },
+//                         { name: "Documentation" }
+//                     ]
+//                 }
+//             ]
+//         },
+//         {
+//             name: "opt",
+//             contents: []
+//         },
+//         {
+//             name: "Users",
+//             contents: [
+//                 { name: "pavanpodila" },
+//                 { name: "admin" },
+//                 { name: "test-user" }
+//             ]
+//         }
+//     ]
+// };
+
+var treeData;
+
 var url = $(location).attr('href');
   var split = url.split('/');
   var id = split[split.length - 1];
-d3.json("/apollo/api/node/managed/"+id, function(error, treeData) { 
-// ************** Generate the tree diagram	 *****************
-var margin = {top: 20, right: 120, bottom: 20, left: 120},
-	width = 1400 - margin.right - margin.left,
-	height = 700 - margin.top - margin.bottom;
+  d3.json("/apollo/api/node/managed/"+id, function(error, treeData) { 
 
-	
-var i = 0;
+function visit(parent, visitFn, childrenFn)
+{
+    if (!parent) return;
 
-var tree = d3.layout.tree()
-	.size([height, width]);
+    visitFn(parent);
 
-var diagonal = d3.svg.diagonal()
-	.projection(function(d) { return [d.y, d.x]; });
-
-var svg = d3.select(".block_edge").append("svg")
-	.attr("width", width + margin.right + margin.left)
-	.attr("height", height + margin.top + margin.bottom)
-  .append("g")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-root = treeData;
-  
-update(root);
-
-function update(source) {
-
-  // Compute the new tree layout.
-  var nodes = tree.nodes(root).reverse(),
-	  links = tree.links(nodes);
-
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 180; });
-
-  // Declare the nodes…
-  var node = svg.selectAll("g.node")
-	  .data(nodes, function(d) { return d.id || (d.id = ++i); });
-
-  // Enter the nodes.
-  var nodeEnter = node.enter().append("g")
-	  .attr("class", "node")
-	  .attr("transform", function(d) { 
-		  return "translate(" + d.y + "," + d.x + ")"; });
-
-  nodeEnter.append("circle")
-	  .attr("r", 10)
-	  .style("fill", "#fff");
-
-  nodeEnter.append("text")
-	  .attr("x", function(d) { 
-		  return d.children || d._children ? -13 : 13; })
-	  .attr("dy", ".35em")
-	  .attr("text-anchor", function(d) { 
-		  return d.children || d._children ? "end" : "start"; })
-	  .text(function(d) { return d.name; })
-	  .style("fill-opacity", 1);
-
-  // Declare the links…
-  var link = svg.selectAll("path.link")
-	  .data(links, function(d) { return d.target.id; });
-
-  // Enter the links.
-  link.enter().insert("path", "g")
-	  .attr("class", "link treelink")
-	  .attr("d", diagonal);
+    var children = childrenFn(parent);
+    if (children) {
+        var count = children.length;
+        for (var i = 0; i < count; i++) {
+            visit(children[i], visitFn, childrenFn);
+        }
+    }
 }
-});
+
+buildTree(".block_edge");
+
+function buildTree(containerName, customOptions)
+{
+    // build the options object
+    var options = $.extend({
+        nodeRadius: 5, fontSize: 12
+    }, customOptions);
+
+    
+    // Calculate total nodes, max label length
+    var totalNodes = 0;
+    var maxLabelLength = 0;
+    visit(treeData, function(d)
+    {
+        totalNodes++;
+        maxLabelLength = Math.max(d.name.length, maxLabelLength);
+    }, function(d)
+    {
+        return d.children && d.children.length > 0 ? d.children: null;
+    });
+
+    // size of the diagram
+    var size = { width:$(containerName).outerWidth() *1.25, height: totalNodes * 15};
+    var tree = d3.layout.tree()
+        .sort(null)
+        .size([size.height, size.width - maxLabelLength*options.fontSize])
+        .children(function(d)
+        {
+            return (!d.children || d.children.length === 0) ? null : d.children;
+        });
+
+    var nodes = tree.nodes(treeData);
+    var links = tree.links(nodes);
+
+    
+    /*
+        <svg>
+            <g class="container" />
+        </svg>
+     */
+    var layoutRoot = d3.select(containerName)
+        .append("svg:svg").attr("width", size.width).attr("height", size.height)
+        .append("svg:g")
+        .attr("class", "container")
+        .attr("transform", "translate(" + maxLabelLength + ",0)");
+
+
+    // Edges between nodes as a <path class="link" />
+    var link = d3.svg.diagonal()
+        .projection(function(d)
+        {
+            return [d.y, d.x];
+        });
+
+    layoutRoot.selectAll("path.link")
+        .data(links)
+        .enter()
+        .append("svg:path")
+        .attr("class", "link treelink")
+        .attr("d", link);
+
+
+    /*
+        Nodes as
+        <g class="node">
+            <circle class="node-dot" />
+            <text />
+        </g>
+     */
+    var nodeGroup = layoutRoot.selectAll("g.node")
+        .data(nodes)
+        .enter()
+        .append("svg:g")
+        .attr("class", "node")
+        .attr("transform", function(d)
+        {
+            return "translate(" + d.y + "," + d.x + ")";
+        });
+
+    nodeGroup.append("svg:circle")
+        .attr("class", "node-dot")
+        .attr("r", options.nodeRadius);
+
+    nodeGroup.append("svg:text")
+        .attr("text-anchor", function(d)
+        {
+            if(d == nodes[0])
+                {return "start"}
+            else
+            {
+                return d.children ? "end" : "start";
+            }
+        })
+        .attr("dx", function(d)
+        {
+            var gap = 2 * options.nodeRadius;
+            if(d == nodes[0])
+                {return gap}
+            return d.children ? -gap : gap;
+        })
+        .attr("dy", 3)
+        .text(function(d)
+        {
+            return d.name;
+        });
+
+}});

3ebf8eed00cf7e16d469c6466a533b18ac926342
sanjithdavid <sdavid@deloitte.com>
2015-03-02 17:09:12 -0500

    TEMP changes for oAuth properties to point to Dev

--- src/server/lib/oAuthProperties.js
+++ src/server/lib/oAuthProperties.js
@@ -4,17 +4,17 @@
 module.exports = {
     authProperties : {
 
-    'facebookAuth' : {   //LOCALHOST
-        'clientID'      : '1429109474024840', // your App ID
-        'clientSecret'  : '71ce33ce1aaa2da3d859d78a3848c164', // your App Secret
-        'callbackURL'   : 'http://localhost:8089/apollo/auth/facebook/callback'
-    },
-
-    // 'facebookAuth' : {   //Apollo Dev
-    //     'clientID'      : '656663327793594', // your App ID
-    //     'clientSecret'  : 'c798e54fa699da3041de84d64f492b39', // your App Secret
-    //     'callbackURL'   : 'http://apollodev.phiresearchlab.org/apollo/auth/facebook/callback'
+    // 'facebookAuth' : {   //LOCALHOST
+    //     'clientID'      : '1429109474024840', // your App ID
+    //     'clientSecret'  : '71ce33ce1aaa2da3d859d78a3848c164', // your App Secret
+    //     'callbackURL'   : 'http://localhost:8089/apollo/auth/facebook/callback'
     // },
+
+    'facebookAuth' : {   //Apollo Dev
+        'clientID'      : '656663327793594', // your App ID
+        'clientSecret'  : 'c798e54fa699da3041de84d64f492b39', // your App Secret
+        'callbackURL'   : 'http://apollodev.phiresearchlab.org/apollo/auth/facebook/callback'
+    }
 
     // 'twitterAuth' : {
     //     'consumerKey'       : 'your-consumer-key-here',

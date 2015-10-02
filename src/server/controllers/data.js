025ed7f572c30e58270bade2687c3a1bee80f221
Michael Ta <technical.ta@gmail.com>
2015-10-02 11:43:46 -0400

    pending upload to mysql db - not working

--- src/server/controllers/data.js
+++ src/server/controllers/data.js
@@ -1,13 +1,27 @@
 var parse =require('csv');
 var mysql = require('../lib/mysqlConnection');
-
 var fs = require('fs');
 
 
 exports.upload = function(req, res){
 	//TODO
 	console.log('FILES',req.files);
+	var filePath = req.files.file.path;
+	var originalFileName = req.files.file.originalFilename;
+	originalFileName =  originalFileName.substring(0,originalFileName.indexOf('.'));
+
+	console.log('filepath', filePath);
 	res.send('done');
+
+	var sqlImport = "load data local infile ? into table ?? fields terminated by ','";
+
+	mysql.query(sqlImport,[filePath, originalFileName], function(err, result) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log(result);
+		}
+	});
 };
 
 

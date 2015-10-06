e51127db27bf4079c7357564b1cce321ccc22d0e
Michael Ta <technical.ta@gmail.com>
2015-10-06 15:51:39 -0400

    pending data upload

--- src/server/controllers/data.js
+++ src/server/controllers/data.js
@@ -1,27 +1,39 @@
 
+var parse =require('csv-parse');
 var mysql = require('../lib/mysqlConnection');
 var fs = require('fs');
+var neodb = require('../lib/neo4jConnection');
 
 
 exports.upload = function(req, res){
 	//TODO
-	console.log('FILES',req.files);
+	//console.log('FILES',req.files);
 	var filePath = req.files.file.path;
 	var originalFileName = req.files.file.originalFilename;
 	originalFileName =  originalFileName.substring(0,originalFileName.indexOf('.'));
+	var input = fs.createReadStream(filePath);
 
-	console.log('filepath', filePath);
-	res.send('done');
+	console.log('input',input);
+	//console.log('filepath', filePath);
+	
+	
+	var parser = parse(function(err,data){
+		console.log('callback data', data[0]);
+	});
+
+	fs.createReadStream(filePath).pipe(parser); 
 
 	var sqlImport = "load data local infile ? into table ?? fields terminated by ','";
 
-	mysql.query(sqlImport,[filePath, originalFileName], function(err, result) {
-		if(err) {
-			console.log(err);
-		} else {
-			console.log(result);
-		}
-	});
+	// mysql.query(sqlImport,[filePath, originalFileName], function(err, result) {
+	// 	if(err) {
+	// 		console.log(err);
+	// 	} else {
+	// 		console.log(result);
+	// 	}
+	// });
+
+	res.send('done');
 };
 
 

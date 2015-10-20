ccbdd61afb3a1932fe075033fa223991eea45a71
Michael Ta <technical.ta@gmail.com>
2015-10-20 13:42:50 -0400

    remove comment/used font awesome icon

--- src/public/app/controllers/previewGridCtrl.js
+++ src/public/app/controllers/previewGridCtrl.js
@@ -1,6 +1,5 @@
 angular.module('jupiterApp').controller('previewGridCtrl', function($scope,$modal, $modalInstance, nodeId, $http) {
 	$scope.previewData;
-	console.log(nodeId);
 
 	$http.get('/api/getDataFile'+nodeId).then(function(res) {
 		$scope.previewData = res.data;
